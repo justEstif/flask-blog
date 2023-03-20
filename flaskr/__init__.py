ce3bf308ec9d26aa9bb8ad572433cb164e2accb7
Estifanos <estifanosbeyene21@gmail.com>
2023-03-20 14:44:28 -0400

    feat: create auth blueprint with register and login view

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -30,4 +30,9 @@ def create_app(test_config=None):
     # database
     from . import db
     db.init_app(app)
+
+    # blueprints
+    from . import auth
+    app.register_blueprint(auth.bp)
+
     return app
